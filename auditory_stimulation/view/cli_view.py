5ca5582944385922943c4d762378ad925f1252f6
michalrzak <michal.r.zak@protonmail.com>
2024-02-08 14:19:11 +0100

    changed the structure of the view

--- auditory_stimulation/view/cli_view.py
+++ auditory_stimulation/view/cli_view.py
@@ -1,29 +1,17 @@
 from typing import Any
 
+from auditory_stimulation.model.experiment_state import EExperimentState
 from auditory_stimulation.model.model_update_identifier import EModelUpdateIdentifier
 from auditory_stimulation.view.view import AView
 
 
 class CLIView(AView):
 
-    # TODO: change type
-    def __update_new_prompt(self, data: Any) -> None:
+    def _update_new_prompt(self, data: str) -> None:
         print(f"Prompt: {data}")
 
-    # TODO: change type
-    def __update_experiment_state_changed(self, data: Any) -> None:
+    def _update_experiment_state_changed(self, data: EExperimentState) -> None:
         print(f"New State: {data}")
-
-    def update(self, data: Any, identifier: EModelUpdateIdentifier) -> None:
-        if identifier == EModelUpdateIdentifier.NEW_PROMPT:
-            # TODO: assert type is correct
-            self.__update_new_prompt(data)
-        elif identifier == EModelUpdateIdentifier.EXPERIMENT_STATE_CHANGED:
-            # TODO: assert type is correct
-            self.__update_experiment_state_changed(data)
-        else:
-            # this should never happen
-            assert False
 
     def get_confirmation(self) -> bool:
         input("Please press the enter key")
