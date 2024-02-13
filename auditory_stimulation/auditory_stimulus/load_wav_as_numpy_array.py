ad4c90abfb63350bc84ae9adb9156e8103a3bb78
michalrzak <michal.r.zak@protonmail.com>
2024-02-13 21:31:59 +0100

    added function to load a wav file as a numpy array

---
+++ auditory_stimulation/auditory_stimulus/load_wav_as_numpy_array.py
@@ -0,0 +1,15 @@
+import numpy as np
+import wave
+
+from auditory_stimulation.auditory_stimulus.auditory_stimulus import Audio
+
+
+def load_wav_as_numpy_array(wav_path: str) -> Audio:
+    with wave.open(wav_path) as f:
+        buffer = f.readframes(f.getnframes())
+        bit_depth = f.getsampwidth() * 8
+        frequency = f.getframerate()
+        interleaved = np.frombuffer(buffer, dtype=f"int{f.getsampwidth() * 8}")
+        audio_data = np.reshape(interleaved, (-1, f.getnchannels()))
+
+    return Audio(audio_data.astype(np.float32) / 2 ** (bit_depth - 1), frequency)
