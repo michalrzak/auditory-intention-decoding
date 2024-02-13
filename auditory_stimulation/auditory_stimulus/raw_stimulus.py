9e2620732d26eb03ad02e1d3d61d565b2be6429d
michalrzak <michal.r.zak@protonmail.com>
2024-02-13 14:43:57 +0100

    fixed typehints

--- auditory_stimulation/auditory_stimulus/raw_stimulus.py
+++ auditory_stimulation/auditory_stimulus/raw_stimulus.py
@@ -1,8 +1,8 @@
-from typing import Any, List, Tuple
+from typing import List, Tuple
 
 from auditory_stimulation.auditory_stimulus.auditory_stimulus import AAuditoryStimulus, Audio
 
 
 class RawStimulus(AAuditoryStimulus):
-    def _create_modified_audio(self, audio: Audio, stimuli_intervals: List[Tuple[float, float]]) -> Any:
+    def _create_modified_audio(self, audio: Audio, stimuli_intervals: List[Tuple[float, float]]) -> Audio:
         return audio
