6fcbfb3c4ebab24f82fd53da26b041a98308f0c5
michalrzak <michal.r.zak@protonmail.com>
2024-02-13 15:36:09 +0100

    updated sound player to new audio type

--- auditory_stimulation/auditory_stimulus/sound_players.py
+++ auditory_stimulation/auditory_stimulus/sound_players.py
@@ -4,26 +4,27 @@ from psychopy.sound.backend_pygame import SoundPygame
 from auditory_stimulation.auditory_stimulus.auditory_stimulus import Audio
 
 
-def psychopy_player(audio: Audio, sampling_rate: int = 44100) -> None:
+def psychopy_player(audio: Audio) -> None:
     """ Plays the given audio. Function is blocking and returns after the audio has finished playing
 
-    :param audio: Nx2 dimensional numpy array of the audio. Audio needs to be in the range [-1, 1]
-    :param sampling_rate: integer of the sampling rate of the audio. Default is 44100.
+    :param audio: a dataclass consisting of
+     * audio: Nx2 dimensional numpy array of the audio. Audio needs to be in the range [-1, 1]
+     * sampling frequency: positive integer
     :return:
     """
 
-    if audio.shape[1] != 2:
+    if audio.audio.shape[1] != 2:
         raise ValueError("The audio has to be stereo! I.e. in the shape Nx2")
 
-    if audio.dtype != np.float32:
+    if audio.audio.dtype != np.float32:
         raise ValueError("The audio has to be of the type np.float32!")
 
-    assert np.max(audio) <= 1
-    assert np.min(audio) >= -1
+    assert np.max(audio.audio) <= 1
+    assert np.min(audio.audio) >= -1
 
-    if sampling_rate <= 0:
+    if audio.sampling_frequency <= 0:
         raise ValueError("Sampling rate has to be > 0")
 
     # use the pygame backend, as it allows to play sounds from arrays, not only from files
-    sound = SoundPygame(value=audio)
+    sound = SoundPygame(value=audio.audio)
     sound.play()
