4d3d5ea9faebe2bf37b5cef2579da37663ee1bb3
michalrzak <michal.r.zak@protonmail.com>
2024-02-26 16:27:22 +0100

    changed ASSR to use dependency injection for its stimulus generation

--- tests/auditory_stimulus/assr_stimulus_test.py
+++ tests/auditory_stimulus/assr_stimulus_test.py
@@ -1,8 +1,16 @@
 import numpy as np
 import pytest
+import numpy.typing as npt
 
-from auditory_stimulation.auditory_stimulus.assr_stimulus import ASSRStimulus
+from auditory_stimulation.auditory_stimulus.assr_stimulus import ASSRStimulus, clicking_signal
 from tests.auditory_stimulus.stimulus_test_helpers import get_mock_audio, get_mock_audio_player, get_mock_ones_audio
+
+
+def mock_stimulus_generation(length: int, frequency: int, sampling_frequency: int) -> npt.NDArray[np.float32]:
+    """Generates a mock stimulus generation function. consisting of only -1. -1 is picked so that the signal is
+    modified.
+    """
+    return -np.ones(length, dtype=np.float32)
 
 
 def test_ASSRStimulus_create_validCall_audioShouldBeModified():
@@ -15,7 +23,8 @@ def test_ASSRStimulus_create_validCall_audioShouldBeModified():
     stimulus = ASSRStimulus(audio,
                             [(0, n_input / sampling_frequency)],
                             get_mock_audio_player(),
-                            stimulus_frequency)
+                            stimulus_frequency,
+                            mock_stimulus_generation)
     stimulus.create()
 
     modified_audio = stimulus.modified_audio
@@ -38,7 +47,8 @@ def test_ASSRStimulus_create_validCall_audioShouldBeModifiedToHalfPoint():
     stimulus = ASSRStimulus(audio,
                             [(0, n_input / sampling_frequency / 2)],
                             get_mock_audio_player(),
-                            stimulus_frequency)
+                            stimulus_frequency,
+                            mock_stimulus_generation)
     stimulus.create()
 
     modified_audio = stimulus.modified_audio
@@ -60,7 +70,11 @@ def test_ASSRStimulus_invalidFrequency_doesNotDivide_shouldThrow():
     stimulus_frequency = 5
 
     with pytest.raises(ValueError):
-        stim = ASSRStimulus(audio, [(0, 1)], get_mock_audio_player(), stimulus_frequency)
+        stim = ASSRStimulus(audio,
+                            [(0, 1)],
+                            get_mock_audio_player(),
+                            stimulus_frequency,
+                            mock_stimulus_generation)
 
 
 def test_ASSRStimulus_invalidFrequency_0_shouldThrow():
@@ -68,7 +82,11 @@ def test_ASSRStimulus_invalidFrequency_0_shouldThrow():
     stimulus_frequency = 0
 
     with pytest.raises(ValueError):
-        stim = ASSRStimulus(audio, [(0, 1)], get_mock_audio_player(), stimulus_frequency)
+        stim = ASSRStimulus(audio,
+                            [(0, 1)],
+                            get_mock_audio_player(),
+                            stimulus_frequency,
+                            mock_stimulus_generation)
 
 
 def test_ASSRStimulus_invalidFrequency_negative_shouldThrow():
@@ -76,7 +94,11 @@ def test_ASSRStimulus_invalidFrequency_negative_shouldThrow():
     stimulus_frequency = -1
 
     with pytest.raises(ValueError):
-        stim = ASSRStimulus(audio, [(0, 1)], get_mock_audio_player(), stimulus_frequency)
+        stim = ASSRStimulus(audio,
+                            [(0, 1)],
+                            get_mock_audio_player(),
+                            stimulus_frequency,
+                            mock_stimulus_generation)
 
 
 def test_ASSRStimulus_create_validCall():
@@ -86,31 +108,29 @@ def test_ASSRStimulus_create_validCall():
 
     stimulus_frequency = 5
 
-    stim = ASSRStimulus(audio, [(0, n_input / sampling_frequency)], get_mock_audio_player(), stimulus_frequency)
+    stim = ASSRStimulus(audio,
+                        [(0, n_input / sampling_frequency)],
+                        get_mock_audio_player(),
+                        stimulus_frequency,
+                        mock_stimulus_generation)
     stim.create()
 
-
-def test_ASSRStimulus_create_validCall_shouldHaveCorrectFrequency():
+def test_clickingStimulus_validCall_shouldHaveCorrectFrequency():
     epsilon = 0.1
 
     sampling_frequencies = [12, 20, 24, 60]
-    stimulus_frequencies = [3,   5,  4, 15]
+    stimulus_frequencies = [3, 5, 4, 15]
 
-    n_input = 1000
+    length = 1000
 
     for sampling_frequency, stimulus_frequency in zip(sampling_frequencies, stimulus_frequencies):
-        audio = get_mock_ones_audio(n_input, sampling_frequency)
+        modulating_stimulus = clicking_signal(length, stimulus_frequency, sampling_frequency)
 
-        stim = ASSRStimulus(audio, [(0, n_input / sampling_frequency)], get_mock_audio_player(), stimulus_frequency)
-        stim.create()
-
-        modified_audio = stim.modified_audio
-
-        modified_audio_spectrum = np.abs(np.real(np.fft.fftshift(np.fft.fft(modified_audio.audio[:, 0]))))
+        modulating_stimulus_spectrum = np.abs(np.real(np.fft.fftshift(np.fft.fft(modulating_stimulus))))
         # cut of the irrelevant half of the spectrum
-        modified_audio_spectrum = modified_audio_spectrum[modified_audio_spectrum.shape[0] // 2:]
+        modulating_stimulus_spectrum = modulating_stimulus_spectrum[modulating_stimulus_spectrum.shape[0] // 2:]
 
-        peak = np.argmax(modified_audio_spectrum)
-        peak_frequency = peak * sampling_frequency / n_input
+        peak = np.argmax(modulating_stimulus_spectrum)
+        peak_frequency = peak * sampling_frequency / length
 
         assert peak_frequency - epsilon <= stimulus_frequency <= peak_frequency + epsilon
