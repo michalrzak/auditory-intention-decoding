529550fb8fcfe25ce392b1ef3acd576291c5e8a3
michalrzak <michal.r.zak@protonmail.com>
2024-02-29 10:49:05 +0100

    Added a text prompt to psychopy vew prompting the user to press space

--- auditory_stimulation/view/psychopy_view.py
+++ auditory_stimulation/view/psychopy_view.py
@@ -1,4 +1,4 @@
-from typing import Callable, Protocol, Dict, Optional
+from typing import Callable, Protocol, Dict, Optional, Tuple, List
 
 import psychopy
 import psychopy.visual
@@ -10,11 +10,15 @@ from auditory_stimulation.stimulus import CreatedStimulus
 from auditory_stimulation.view.view import AView
 
 LETTER_SIZE = 0.05
-TEXT_BOX_POSITION = (0, 0)
-TEXT_BOX_SIZE = (0.5, 0.5),
 TEXT_BOX_COLOR = 1.
 TEXT_BOX_COLOR_SPACE = 'rgb'
-TEXT_BOX_FONT = 'Caladea'
+
+EXPERIMENT_STATE_TEXT_BOX_POSITION = (0, 0)
+EXPERIMENT_STATE_TEXT_BOX_SIZE = (0.5, 0.5)
+
+CONFIRMATION_TEXT = "Please press 'space' to continue"
+CONFIRMATION_TEXT_BOX_POSITION = (0, -0.8)
+CONFIRMATION_TEXT_BOX_SIZE = (0.5, 0.2)
 
 
 class Drawable(Protocol):
@@ -25,6 +29,9 @@ class Drawable(Protocol):
 class PsychopyView(AView):
     """A view, implementing the psychopy frontend.
     """
+    __window: psychopy.visual.Window
+    __keyboard: psychopy.hardware.keyboard.Keyboard
+    __draw_buffer: List[Drawable]
 
     def __init__(self,
                  sound_player: Callable[[Audio], None],
@@ -39,39 +46,50 @@ class PsychopyView(AView):
         super().__init__(sound_player, experiment_texts)
         self.__window = window
         self.__keyboard = keyboard.Keyboard()
+        self.__draw_buffer = []
 
-    def __try_to_quit(self):
+    def __try_to_quit(self) -> None:
         if len(self.__keyboard.getKeys(["escape"])) != 0:
             self.close_view()
+
+    def __draw(self, item: Drawable, clear_buffer: bool):
+        if clear_buffer:
+            self.__draw_buffer = []
+
+        self.__draw_buffer.append(item)
+
+        for buffered_item in self.__draw_buffer:
+            buffered_item.draw()
+
+        self.__window.flip()
 
     def _update_new_stimulus(self, stimulus: CreatedStimulus) -> None:
         self.__try_to_quit()
 
-        prompt = self.__create_text_box(stimulus.prompt)
-
-        prompt.draw()
-        self.__window.flip()
+        prompt = self.__create_text_box(stimulus.prompt, EXPERIMENT_STATE_TEXT_BOX_POSITION,
+                                        EXPERIMENT_STATE_TEXT_BOX_SIZE)
+        self.__draw(prompt, True)
 
         self._sound_player(stimulus.modified_audio)
 
     def _update_new_primer(self, primer: str) -> None:
         self.__try_to_quit()
 
-        prompt = self.__create_text_box(primer)
-
-        prompt.draw()
-        self.__window.flip()
+        prompt = self.__create_text_box(primer, EXPERIMENT_STATE_TEXT_BOX_POSITION, EXPERIMENT_STATE_TEXT_BOX_SIZE)
+        self.__draw(prompt, True)
 
     def _update_experiment_state_changed(self, data: EExperimentState) -> None:
         self.__try_to_quit()
 
-        text = self.__create_text_box(self._experiment_texts[data])
-
-        text.draw()
-        self.__window.flip()
+        text = self.__create_text_box(self._experiment_texts[data], EXPERIMENT_STATE_TEXT_BOX_POSITION,
+                                      EXPERIMENT_STATE_TEXT_BOX_SIZE)
+        self.__draw(text, True)
 
     def get_confirmation(self) -> bool:
         self.__try_to_quit()
+
+        text = self.__create_text_box(CONFIRMATION_TEXT, CONFIRMATION_TEXT_BOX_POSITION, CONFIRMATION_TEXT_BOX_SIZE)
+        self.__draw(text, False)
 
         self.__keyboard.clearEvents()  # clear keys in case the key was already pressed before
         while len(self.__keyboard.getKeys(["space"])) == 0:
@@ -84,15 +102,14 @@ class PsychopyView(AView):
 
         psychopy.core.wait(secs)
 
-    def __create_text_box(self, text: str) -> Drawable:
+    def __create_text_box(self, text: str, position: Tuple[float, float], size: Tuple[float, float]) -> Drawable:
         return psychopy.visual.TextBox2(win=self.__window,
                                         text=text,
                                         letterHeight=LETTER_SIZE,
-                                        pos=TEXT_BOX_POSITION,
-                                        size=TEXT_BOX_SIZE,
+                                        pos=position,
+                                        size=size,
                                         color=1.,
-                                        colorSpace=TEXT_BOX_COLOR_SPACE,
-                                        font=TEXT_BOX_FONT)
+                                        colorSpace=TEXT_BOX_COLOR_SPACE)
 
     def close_view(self):
         """Closes the view properly."""
