aa66a18c0249880307d112d622c21e4a1297cb00
michalrzak <michal.r.zak@protonmail.com>
2024-03-04 15:29:48 +0100

    added post-init checks to audio and updated failing test

--- auditory_stimulation/audio.py
+++ auditory_stimulation/audio.py
@@ -10,11 +10,21 @@ class Audio:
     audio: npt.NDArray[np.float32]
     sampling_frequency: int
 
+    def __post_init__(self):
+        if len(self.audio.shape) != 2 or self.audio.shape[1] != 2:
+            raise ValueError("The supplied audio must be of shape Nx2!")
+
+        if self.sampling_frequency <= 0:
+            raise ValueError("The sampling frequency must be a positive integer!")
+
     def __copy__(self) -> "Audio":
         return Audio(np.copy(self.audio), self.sampling_frequency)
 
     def __eq__(self, other: "Audio") -> bool:
         return np.all(self.audio == other.audio) and self.sampling_frequency == other.sampling_frequency
+
+    def __repr__(self) -> str:
+        return f"Audio(audio-shape={self.audio.shape}, sampling_frequency={self.sampling_frequency})"
 
 
 def load_wav_as_numpy_array(wav_path: str) -> Audio:
