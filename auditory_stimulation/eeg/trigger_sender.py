8244f191629b94a800ad657c4a6550c445d79eaa
michalrzak <michal.r.zak@protonmail.com>
2024-03-21 20:53:55 +0100

    updated trigger sending

--- auditory_stimulation/eeg/trigger_sender.py
+++ auditory_stimulation/eeg/trigger_sender.py
@@ -9,35 +9,41 @@ from auditory_stimulation.model.model import AObserver
 from auditory_stimulation.model.model_update_identifier import EModelUpdateIdentifier
 from auditory_stimulation.model.stimulus import CreatedStimulus
 
-TIMEOUT = 5
+
+class ThreadDiedException(Exception):
+    ...
 
 
 class ATriggerSender(AObserver):
     __trigger_queue: queue.Queue
 
+    __thread_timout: int
     __thread: threading.Thread
     __exit_flag: bool
 
-    def __init__(self) -> None:
+    def __init__(self, thread_timout: int) -> None:
+        self.__thread_timeout = thread_timout
+
         self.__thread = threading.Thread(target=self.__trigger_worker)
         self.__trigger_queue = queue.Queue()
         self.__exit_flag = False
 
-    def start(self):
-        self.__thread.start()
-
     def __trigger_worker(self):
         while not self.__exit_flag:
             try:
-                item = self.__trigger_queue.get(timeout=TIMEOUT)
-            except queue.Empty as e:
+                item = self.__trigger_queue.get(timeout=self.__thread_timout)
+            except queue.Empty:
                 continue
             self._send_trigger(item)
             self.__trigger_queue.task_done()
 
-    def __queue_trigger(self, trigger: ETrigger, secs: float) -> None:
+    def __queue_trigger(self, trigger: ETrigger, offset: float = 0) -> None:
+        if offset == 0:
+            self.__trigger_queue.put(trigger)
+            return
+
         def wait_and_put():
-            time.sleep(secs)
+            time.sleep(offset)
             self.__trigger_queue.put(trigger)
 
         thread = threading.Thread(target=wait_and_put)
@@ -54,8 +60,7 @@ class ATriggerSender(AObserver):
 
     def update(self, data: Any, identifier: EModelUpdateIdentifier) -> None:
         self.__trigger_queue.put(ETrigger.get_trigger(data, identifier))
-        # send the trigger instantly, but on another thread
-        self.__queue_trigger(ETrigger.get_trigger(data, identifier), 0)
+        self.__queue_trigger(ETrigger.get_trigger(data, identifier))
 
         # in case a new stimulus is received, also queue sending trigger after it finishes playing and at the beginning
         #  of each option
@@ -74,6 +79,10 @@ class ATriggerSender(AObserver):
         self.__del__()
 
     def __del__(self):
+        if not self.__thread.is_alive():
+            raise ThreadDiedException("The thread died unexpectedly!")
+
         self.__trigger_queue.join()
         self.__exit_flag = True
         self.__thread.join()
+        assert not self.__thread.is_alive()
