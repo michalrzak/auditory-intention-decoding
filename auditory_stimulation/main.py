06b92c2f547d21f48634b3efda43811048f24b27
michalrzak <michal.r.zak@protonmail.com>
2024-04-04 16:17:14 +0200

    declaring the random number generator of noise tagging in the main file

--- auditory_stimulation/main.py
+++ auditory_stimulation/main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from os import PathLike
 from random import Random
 
+import numpy as np
 import psychopy.visual
 
 from auditory_stimulation.auditory_tagging.assr_tagger import AMTagger, FlippedFMTagger, FMTagger
@@ -22,15 +23,6 @@ from auditory_stimulation.view.psychopy_view import PsychopyView
 from auditory_stimulation.view.sound_players import psychopy_player
 from auditory_stimulation.view.view import ViewInterrupted
 
-TAGGERS = [AMTagger(42, sine_signal),
-           FlippedFMTagger(40, 0.8),
-           NoiseTaggingTagger(44100, 126, 256),
-           FMTagger(40, 100),
-           ShiftSumTagger(40),
-           SpectrumShiftTagger(40),
-           BinauralTagger(40),
-           RawTagger()]
-
 
 def create_directory_if_not_exists(directory: PathLike) -> None:
     directory_path = pathlib.Path(directory)
@@ -39,6 +31,15 @@ def create_directory_if_not_exists(directory: PathLike) -> None:
 
 def main() -> None:
     config = get_configuration_psychopy()
+
+    taggers = [AMTagger(42, sine_signal),
+               FlippedFMTagger(40, 0.8),
+               NoiseTaggingTagger(44100, 126, 256, np.random.default_rng(config.subject_ID)),
+               FMTagger(40, 100),
+               ShiftSumTagger(40),
+               SpectrumShiftTagger(40),
+               BinauralTagger(40),
+               RawTagger()]
 
     day_id = f"{datetime.today().strftime('%Y-%m-%d-%H-%M-%S')}_subject-{config.subject_ID}"
     logging_folder = config.logging_directory_path / day_id
@@ -49,7 +50,7 @@ def main() -> None:
 
     # stimuli = load_stimuli(pathlib.Path("auditory_stimulation/stimuli.yaml"))
     stimuli = generate_created_stimuli(n_repetitions=config.repetitions,
-                                       taggers=TAGGERS,
+                                       taggers=taggers,
                                        n_stimuli=config.n_stimuli,
                                        pause_secs=config.pause_secs,
                                        number_stimuli_interval=config.stimuli_numbers_interval,
@@ -72,7 +73,7 @@ def main() -> None:
         model.register(ts, 1)
         experiment = Experiment(model,
                                 view,
-                                len(TAGGERS),
+                                len(taggers),
                                 config.resting_state_secs,
                                 config.primer_secs,
                                 config.break_secs)
